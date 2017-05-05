1522da7ded06ca6220474d0155b6382ff84f1e28
Aaron-zheng <fucheng.zheng@gmail.com>
2017-05-05 17:52:06 +0800

    情景3

--- src/main/resources/shell/5.jks.sh
+++ src/main/resources/shell/5.jks.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 # create truststore, input 'certjks'
+cd /root/ca
+mkdir jks
+keytool -import -trustcacerts -alias root -file certs/ca.cert.pem -keystore jks/ca.cert.jks
+
+# create truststore, input 'certjks'
 cd /root/ca/intermediate
+mkdir jks
 keytool -import -trustcacerts -alias root -file certs/intermediate.cert.pem -keystore jks/intermediate.cert.jks
